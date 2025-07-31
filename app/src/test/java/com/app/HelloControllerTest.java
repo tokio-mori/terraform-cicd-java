c3edd67abf43a23e58fda613026c29a3c68c0a24
tokio-mori <tokio07181120@gmail.com>
2025-08-01 01:12:41 +0900

    update java test

---
+++ app/src/test/java/com/app/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package com.app;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHello() throws Exception {
+        mockMvc.perform(get("/hello"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Hello World!"));
+    }
+}
